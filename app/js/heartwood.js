b4a3171c814bd9067459605ee28c9227f4877bf7
chiika <chiika@Annas-MacBook-Air.local>
2015-02-07 00:24:45 +0900

    mycket

--- app/js/heartwood.js
+++ app/js/heartwood.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-    var heartwoodHappens = moment("2015-02-15 18:00");
+    var heartwoodHappens = moment("2015-08-21 12:00");
     console.log(heartwoodHappens);
     var now = moment();
     console.log(now);
@@ -11,4 +11,18 @@ $(document).ready(function()
         countdown : true,
         showSeconds : true
     });
+	
 })
+$(function(){
+	var size = 1.6;
+	$("#firstline").fitText(size);
+	$("#secondline").fitText(size*1.28);
+	$("#thirdline").fitText(size*1.38);
+	$("#fourthline").fitText(size*1.65);
+	$("#fifthline").fitText(size*2.11);
+	$("#sixthline").fitText(size*2.06);
+	$("#seventhline").fitText(size*2.5);
+	$("#eighthline").fitText(size*2.7);
+	$("#ninthline").fitText(size*2.85);
+	$("#tenthline").fitText(size*2.88);
+});
